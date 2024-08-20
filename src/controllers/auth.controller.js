ac5b0c64c498b0aa4087429c6eb753e20f854f6d
Jona Robledo <jonarobledo.07.jr@outlook.com>
2024-08-20 13:07:34 -0300

    hashing password

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,21 @@
-export const login = (req, res) => {
+import { User } from '../models/user.js'
+
+export const login = async (req, res) => {
 	res.send('response by /login route')
 }
 
-export const register = (req, res) => {
+export const register = async (req, res) => {
 	console.log(req.body)
-	res.send('response by /register route')
+	const { email, password } = req.body
+
+	try {
+		const user = new User({ email, password })
+		await user.save()
+
+		return res.json({ ok: true })
+	} catch (error) {
+		console.log(error)
+	}
+
+	// res.send('response by /register route')
 }
