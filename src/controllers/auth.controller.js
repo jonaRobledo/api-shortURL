5024a0a4f48bba8a0b098a03fd03248ca3f9028d
Jona Robledo <jonarobledo.07.jr@outlook.com>
2025-01-13 01:46:13 -0300

    login finished and generate jwt

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,26 +1,30 @@
+// Import dependencies
+import jwt from 'jsonwebtoken'
+
+// User Model
 import { User } from '../models/User.js'
 
 export const login = async (req, res) => {
-	console.log(req.body)
 	const { email, password } = req.body
 
 	try {
-		const findUser = await User.findOne(email)
-		if (!findUser) throw new Error('This User not Exist')
+		const findUser = await User.findOne({ email })
+		if (!findUser) throw { message: 'Nonexistent User', code: 403 }
 
-		const validatePassword = user.comparePassword(password)
-		if (!validatePassword) throw new Error('Invalid Credentials')
+		const validatePassword = await findUser.comparePassword(password)
+		if (!validatePassword) throw { message: 'Invalid Credentials', code: 403 }
 
-		res.json({ ok: 'Authenticated User' })
+		const token = jwt.sign({ uid: findUser._id }, process.env.JWT_SECRET)
+
+		res.json({ ok: 'Authenticated User', token })
 	} catch (error) {
 		console.log(error)
-		res.status(404).json({ error: error.message })
-		//! Falta resolver las validaciones del /Login
+		// Custom Error handling
+		res.status(error.code).json(error)
 	}
 }
 
 export const register = async (req, res) => {
-	console.log(req.body)
 	const { email, password } = req.body
 
 	try {
